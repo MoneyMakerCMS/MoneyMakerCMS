d219720ea4625b9b2cc45ce477903a0d1e0b1215
ninjaparade <yazjallad@gmail.com>
2016-08-18 20:13:54 -0700

    updated depencancies

--- config/app.php
+++ config/app.php
@@ -176,6 +176,13 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * MoneyMaker Providers
+        */
+        Silber\Bouncer\BouncerServiceProvider::class,
+        Rinvex\Repository\Providers\RepositoryServiceProvider::class,
+
+
     ],
 
     /*
@@ -222,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Bouncer' => Silber\Bouncer\BouncerFacade::class,
 
     ],
 
