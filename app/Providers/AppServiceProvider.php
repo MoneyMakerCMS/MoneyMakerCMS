8180eec06d9d1899a3fc2c5eb041f54e8baa122e
ninjaparade <yazjallad@gmail.com>
2016-08-18 22:00:51 -0700

    added debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
+use Barryvdh\Debugbar\Middleware\Debugbar;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('APP_ENV') && env('APP_DEBUG')) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            AliasLoader::getInstance()->alias('Debugbar', \Barryvdh\Debugbar\Facade::class);
+        }
     }
 }
