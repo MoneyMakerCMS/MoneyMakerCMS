5c6a746ed3ec4732424cf8ea1d031fc4eaa80d89
ninjaparade <yazjallad@gmail.com>
2016-08-26 16:28:55 -0700

    fixed super admin seeding abilities

--- database/seeds/AccessRolesPermissionsSeeder.php
+++ database/seeds/AccessRolesPermissionsSeeder.php
@@ -55,7 +55,7 @@ class AccessRolesPermissionsSeeder extends Seeder
 
     protected function assignAbilitiesToRoles()
     {
-        Bouncer::allow('admin')->to([
+        Bouncer::allow('super-admin')->to([
             'name'  => '*',
             'title' => 'God Mode',
         ], '*');
