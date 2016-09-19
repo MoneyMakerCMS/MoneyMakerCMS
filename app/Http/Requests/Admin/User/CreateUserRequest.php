6e86863deb9fbd3f0d30f10753e5fa083c711216
ninjaparade <yazjallad@gmail.com>
2016-09-18 19:41:04 -0700

    added user create and edit form requests

---
+++ app/Http/Requests/Admin/User/CreateUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Bouncer;
+use App\Models\Access\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Bouncer::allows('create', User::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'                  => 'required',
+            'email'                 => 'required|email|unique:users',
+            'password'              => 'required|alpha_num|min:6|confirmed',
+            'password_confirmation' => 'required|alpha_num|min:6',
+        ];
+    }
+}
