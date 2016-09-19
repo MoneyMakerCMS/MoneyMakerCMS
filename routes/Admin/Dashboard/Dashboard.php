51e5668c620ad3165a7b9f3ddf7164c8170e691f
ninjaparade <yazjallad@gmail.com>
2016-09-18 19:38:14 -0700

    updated web routes

---
+++ routes/Admin/Dashboard/Dashboard.php
@@ -0,0 +1,6 @@
+<?php 
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['web', 'auth', 'permission:view-admin'] ], function () {
+    Route::get('/', 'DashboardController@index')->name('admin.dashboard.index');
+    Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard.index');
+});
