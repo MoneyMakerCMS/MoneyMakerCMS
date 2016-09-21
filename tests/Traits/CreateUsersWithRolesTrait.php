d9f08a470291af7d47cad2cc1cbe49e67e3f9249
ninjaparade <yazjallad@gmail.com>
2016-09-21 13:19:02 -0700

    Z’d by Wathan

---
+++ tests/Traits/CreateUsersWithRolesTrait.php
@@ -0,0 +1,21 @@
+<?php 
+
+trait CreateUsersWithRolesTrait
+{
+    protected $user;
+
+    protected function createUserWith($roles='', $permissions='')
+    {
+        $this->user = factory(\App\Models\Access\User::class)->create();
+
+        return tap($this->user, function ($user) use($roles, $permissions) {
+            collect($roles)->each(function ($role) {
+                $this->user->assign($role);
+            });
+
+            collect($permissions)->each(function ($permission) {
+                $this->user->allow($permission);
+            });
+        });
+    }
+}
