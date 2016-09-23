9d05f30c9abc7c182eec0a6c23af578d1bd72bdc
ninjaparade <yazjallad@gmail.com>
2016-09-23 14:04:06 -0700

    updated routes template

--- resources/views/pages/routes/routes.blade.php
+++ resources/views/pages/routes/routes.blade.php
@@ -1,6 +1,7 @@
-{!! '<' . '?' . 'p' . 'h' . 'p' !!}
+{!! '<'.'?php' !!}
 @foreach($pages as $page)
 Route::group(['middleware' => [{!!$page->page_middleware!!}] ], function () {
-  	Route::get('{{$page->uri}}', 'Ninjaparade\Pages\Http\Controllers\Frontend\PagesController@index')->name('{{$page->route}}');
+  	Route::get('{{$page->uri}}', 'App\Http\Controllers\Frontend\Pages\PagesController@index')->name('{{$page->route}}');
 });
+
 @endforeach
