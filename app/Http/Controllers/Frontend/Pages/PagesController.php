8e9ebe1950503efbd97970ee15a2a7ab189a7837
ninjaparade <yazjallad@gmail.com>
2016-09-24 12:27:20 -0700

    updated pages controller
    added if to query for page
    abort 404 if page isn;t found

--- app/Http/Controllers/Frontend/Pages/PagesController.php
+++ app/Http/Controllers/Frontend/Pages/PagesController.php
@@ -2,23 +2,24 @@
 
 namespace App\Http\Controllers\Frontend\Pages;
 
-
 use App\Http\Controllers\Controller;
 use App\Repositories\Pages\PagesRepository;
 
 class PagesController extends Controller
 {
-	private $pages;
+    private $pages;
 
-	public function __construct(PagesRepository $pages)
-	{
-		$this->pages = $pages;
-	}
-
-
-	public function index()
-	{
-		
-	}
-
+    public function __construct(PagesRepository $pages)
+    {
+        $this->pages = $pages;
+    }
+    
+    public function index()
+    {
+        if ($page = $this->pages->render(request()->path())) {
+            return view('pages.views.view')->with(compact('page'));
+        }
+        
+        abort(404);
+    }
 }
