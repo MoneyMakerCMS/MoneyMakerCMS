69e138d14e4c3adfc011d2f52145d4611c91b1cf
ninjaparade <yazjallad@gmail.com>
2016-09-24 08:52:22 -0700

    removed route file

--- routes/Frontend/Pages/Pages.php
+++
@@ -1,4 +0,0 @@
-<?php
-Route::group(['middleware' => ["web","auth"] ], function () {
-  	Route::get('/', 'Ninjaparade\Pages\Http\Controllers\Frontend\PagesController@index')->name('frontend.index');
-});
