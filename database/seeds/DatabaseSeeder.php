fcfce88f2d316b523182b35e5909aea1e0373b17
ninjaparade <yazjallad@gmail.com>
2016-09-26 15:53:15 -0700

    keep this blank

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(AccessRolesPermissionsSeeder::class);
         // $this->call(ContentSeeder::class);
-        $this->call(PagesSeeder::class);
+        // $this->call(PagesSeeder::class);
     }
 }
