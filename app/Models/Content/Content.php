564fa8ee3cae1eddc9e92785aed6d45d8f5de71d
ninjaparade <yazjallad@gmail.com>
2016-09-26 14:09:12 -0700

    reoder use

--- app/Models/Content/Content.php
+++ app/Models/Content/Content.php
@@ -2,13 +2,14 @@
 
 namespace App\Models\Content;
 
-use App\Models\Traits\Render\TableActionsRenderTraite;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\Render\TableActionsRenderTraite;
 
 class Content extends Model
 {
     use TableActionsRenderTraite;
 
     protected $guarded = ['id'];
+    
     protected $adminRouteString = 'admin.content.';
 }
