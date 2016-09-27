89ebb609db76609183b1cc1840f53a4087fba9da
ninjaparade <yazjallad@gmail.com>
2016-09-27 15:34:03 -0700

    clear routes file

--- tests/tmp/routes.php
+++ tests/tmp/routes.php
@@ -1,6 +1,1 @@
 <?php
-
-Route::group(['namespace' => 'Frontend\Pages', 'middleware' => ["web","auth"] ], function () {
-  	Route::get('test-page', 'PagesController@index')->name('frontend.test-page');
-});
-
