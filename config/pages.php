a04044df76b9d7ee4c6a2e6c251f3cef2644796c
ninjaparade <yazjallad@gmail.com>
2016-10-19 17:37:47 -0700

    remove web since all routes in the web.php routes file have web implied

--- config/pages.php
+++ config/pages.php
@@ -22,7 +22,6 @@ return [
      * Default middleware you can apply to a page
      */
     'middleware' => [
-        ['name' => 'Web (default)', 'value' => 'web'],
         ['name' => 'Logged in', 'value' => 'auth'],
         ['name' => 'View Admin', 'value' => 'access.routeNeedsPermission:view-backend'],
     ],
