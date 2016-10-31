62d1ccdf0430f4252ed985a9bb1d5b243829d09f
ninjaparade <yazjallad@gmail.com>
2016-10-31 13:08:59 -0700

    cleaned up page form

--- resources/views/admin/pages/form.blade.php
+++ resources/views/admin/pages/form.blade.php
@@ -7,66 +7,56 @@
 <link href="//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
 @stop
 
-
-{{-- Page --}}
 @section('content')
-<div class="box box-success">
-    <div class="box-body">
-        <div class="box-body" id="pageForm">
-            <h3 class="box-title"></h3>
-            <div class="row nav-tabs-custom">
-                <ul class="nav nav-tabs">
-                    <li class="active"><a href="#page">Page</a></li>
-                    <li><a href="#seo">SEO</a></li>
-                    <li><a href="#media">Media</a></li>
-                </ul>
-                {!! BootForm::openHorizontal(['sm' => [2, 9],'md' => [2, 9]]) !!}
+<form id="page-form" class="form-horizontal" method="POST">
+  {{csrf_field()}}
+  <div class="nav-tabs-custom" style="padding-bottom:20px;">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#page" aria-controls="page" role="tab" data-toggle="tab">Page</a></li>
+      <li role="presentation"><a href="#seo" aria-controls="seo" role="tab" data-toggle="tab">Page Meta</a></li>
+      {{-- <li role="presentation"><a href="#media" aria-controls="media" role="tab" data-toggle="tab">Media</a></li>
+      <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li> --}}
+    </ul>
 
-                <div class="tab-content">
-                    <div class="tab-pane active" id="page">
-                        @include('admin.pages.forms.page')
-
-                    </div>
-                    <div class="tab-pane" id="seo">@include('admin.pages.forms.meta')
-                       <div class="form-group">
-
-
-                       </div>
-                   </div>
-
-
-                   <div class="tab-pane" id="media">{{-- @include('ninjaparade/media::admin.dashboard.ajax-upload') --}}</div>
-               </div>
-           </div>
-       </div>
-       <div class="form-group">
-
-        <label class="col-xs-2 control-label" for="submit">{{{ trans('action.save') }}}</label>
-
-        <div class="col-xs-9">
-            <button type="submit" class="btn btn-success" id="save"><i class="fa fa-save"></i> {{{ "Save" }}}</button>
-        </div>
-        
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div role="tabpanel" class="tab-pane fade in active" id="page">@include('admin.pages.forms.page')</div>
+      <div role="tabpanel" class="tab-pane fade" id="seo">@include('admin.pages.forms.meta')</div>
+      {{-- <div role="tabpanel" class="tab-pane fade" id="media"></div>
+      <div role="tabpanel" class="tab-pane fade" id="settings"></div> --}}
     </div>
+    <div class="form-group">
+      <label class="col-xs-2 control-label" for=""></label>
+      <div class="col-xs-9">
+        <button class="btn btn-default">Save Page</button>
+      </div>
+    </div>
+  </div>
 
-    {!! BootForm::close() !!}
-</div>
-</div>
-
-
-
+</form>
 @stop
 
-
-@section('after-scripts')
 
 @section('after-scripts')
 <script src="//cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
 <script>
-    var Page = {
-        database: @if( old('type', $page->type) === 'database' || old('type', $page->type) === null ) true
-        @else false @endif,
-    };
+  var Page = {
+    database: @if( old('type', $page->type) === 'database' || old('type', $page->type) === null ) true
+    @else false @endif,
+  };
 </script>
 <script src="{{url('javascript/admin/pages.js')}}"></script>
 @stop
+
+{{--
+<div class="tab-pane active" id="page">
+  @include('admin.pages.forms.page')
+
+</div>
+<div class="tab-content">
+  <div class="tab-pane" id="seo">
+     
+  </div>
+
+  --}}
